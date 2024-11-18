354a5536bb1cccb648f885b0ac6c6139fdefdd0f
hinkleydev <azz@hinkley.dev>
2024-11-18 12:04:56 +0000

    Added model and tests

--- tests/database.test.js
+++ tests/database.test.js
@@ -5,11 +5,11 @@ const { Item } = require("../server/models/index.js")
 // Reset database each time
 beforeEach(async function() {
     await sequelize.sync({force: true});
-    //await Item.create(/* TODO:  Add details here of test items */);
+    await Item.create({name: "Shoes", description: "Feet", price: 10.50});
 })
 
 // Sanity check
-test.only("The database connection works", async function() {
+test("The database connection works", async function() {
     const answer = await sequelize.query("SELECT 1+1 AS RESULT");
     expect(answer[0][0].RESULT).toBe(2);
 })
@@ -18,10 +18,13 @@ test.only("The database connection works", async function() {
 
 // Can create items
 test("Items can be created", async function () {
-    const created = await Item.create(/* TODO: Add variables here */);
+    const created = await Item.create({name: "Red shoes", description: "Red feet", price: 20.50, image:"http://example.com"});
     const fetched = await Item.findByPk(2);
-    expect(created).toEqual(fetched);
-    expect(created).toEqual(/* Other values */);
+    expect(created.name).toEqual(fetched.name);
+    expect(fetched.name).toBe("Red shoes");
+    expect(fetched.description).toBe("Red feet");
+    expect(fetched.price).toBe(20.50);
+    expect(fetched.image).toBe("http://example.com")
 })
 
 // --- READ operations --- 
@@ -29,7 +32,11 @@ test("Items can be created", async function () {
 // Check all columns
 test("The database has the correct columns", async function() {
     const result = await Item.findByPk(1);
-    // TODO: Check columns match diagram once created
+    expect(result.id).toBe(1)
+    expect(result.name).toBe("Shoes");
+    expect(result.description).toBe("Feet");
+    expect(result.price).toBe(10.50);
+    // TODO: Add description and image URL
 })
 
 // --- UPDATE operations --- 
@@ -37,9 +44,9 @@ test("The database has the correct columns", async function() {
 // Check editing works correctly
 test("The database gets edited correctly", async function() {
     let result = await Item.findByPk(1);
-    await Item.update({ /* TODO: Add variables here to test */})
+    await result.update({ name: "Blue shoes"})
     result = await Item.findByPk(1);
-    expect(/* TODO: Make comparison */).toBe();
+    expect(result.name).toBe("Blue shoes");
 })
 
 // --- DELETE operations --- 
@@ -49,5 +56,5 @@ test("The database can delete rows", async function () {
     const result = await Item.findByPk(1);
     await result.destroy();
     const deleted = await Item.findByPk(1);
-    expect(deleted).toBeUndefined();    
+    expect(deleted).toBeNull();    
 })
