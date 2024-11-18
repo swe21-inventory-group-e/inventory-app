6cc385d59800117eebee537c3957b9fb74d4244d
jdaly3411 <jdaly3411@gmail.com>
2024-11-18 11:29:51 +0000

    Feat: GET, POST, PUT, DELETE routes completed and tested with postman

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const itemsRouter = require("./items");
 
 // different model routers
 router.use("/items", require("./items"));
+router.use("/items", itemsRouter);
 
 // Test route to ensure the API is working
 router.get("/test", (req, res) => {
