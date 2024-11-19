6e94322d1cb8a83abf92a7e4e22ce82050fb17ba
jdaly3411 <jdaly3411@gmail.com>
2024-11-19 14:20:59 +0000

    FIX: fixed items not rendering

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -33,26 +33,16 @@ function App() {
           <div className="X-button"> _x_</div>
         </div>
         <div className="description_and_image">
-          <img
-            src="https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg"
-            alt="mens clothing"
-          ></img>
-          <p>
-            The color could be slightly different between on the screen and in
-            practice. / Please note that body builds vary by person, therefore,
-            detailed size information should be reviewed below on the product
-            description.
-          </p>
           <ul>
-            {items.map((item) => {
+            {items.map((item) => (
               <li key={item.id}>
                 <h3>{item.name}</h3>
                 <p>{item.description}</p>
                 <p>Price: ${item.price}</p>
                 <p>Category: {item.category}</p>
                 <img src={item.image} alt={item.name} />
-              </li>;
-            })}
+              </li>
+            ))}
           </ul>
         </div>
         <div className="bottom-row">
