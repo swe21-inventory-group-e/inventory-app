44427924f64f6ad83f359b6c3c898e329e90834b
jdaly3411 <jdaly3411@gmail.com>
2024-11-19 14:43:35 +0000

    Added AddPage.js

---
+++ public/react/components/AddPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "./components/Layout";
+
+function AddPage() {
+  return (
+    <Layout>
+      <div className="add-page">
+        <h2>Add New Item</h2>
+      </div>
+    </Layout>
+  );
+}
+
+export default AddPage;
