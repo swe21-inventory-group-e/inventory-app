4c946d642b609b23a4ee4fbc237684858d3d89e0
Riley <rileyeverett16@outlook.com>
2024-11-19 11:05:22 +0000

    front end design for website needs to be hooked up to api

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -9,13 +9,33 @@ function App() {
   useEffect(() => {
     // Fetch the items
   }, []);
-
+  
   return (
     <>
-      <h1>Inventory App</h1>
-      {/* Render the items */}
+      <div class ='websitebanner'>
+      <h1>Marketplace</h1>
+      <div className="buttons">
+      <button>Mens</button>
+      <button>electronics</button>
+      </div>
+      </div>
+      <div className="item">
+        <div className="window-header">
+            <h2>Mens Casual Slim Fit</h2>
+            <div className="X-button"> _x_</div>
+            </div>
+            <div className ='description_and_image'>
+            <img src='https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg' alt='mens clothing'></img>
+            <p>The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.</p>
+            
+            
+            </div>
+            <div className='bottom-row'>
+            <button className='buy-button'>Buy Now</button>
+            <div className="pricetag">Price: $15.99</div>
+            
+          </div></div>
     </>
   );
 }
-
 export default App;
